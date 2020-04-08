ae8e24858639cad37e8776ffe81de9501c7d9102
Sowa <Grimmino@gmail.com>
2020-04-08 21:39:10 +0500

    init

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import 'normalize.css';
+import './sass/style.sass';
+import './js/main';
---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+// let logo = document.querySelector('.logo');
+
+// const activeLogo = (scrollY) => {
+//     scrollY > 200 ? logo.classList.add('logo--active') : logo.classList.remove('logo--active');
+// };
+// activeLogo(window.scrollY);
+
+// window.addEventListener('scroll', () => {
+//     let scrollY = window.scrollY;
+//     activeLogo(scrollY);
+// });
+
+$('.clicker').click(function () {
+    console.log(new Date());
+});
