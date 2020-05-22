835fb754d03bdb6d75520e1c92ff1d64e49eb5f3
Sowa <Grimmino@gmail.com>
2020-05-22 18:28:05 +0500

    rebuild

--- src/js/main.js
+++ src/js/main.js
@@ -24,3 +24,65 @@ burgerBtn.addEventListener("click", () => {
     burgerBtn.classList.toggle("burger--active");
     headerMenu.classList.toggle("menu--active");
 });
+
+//tabs ================================
+let tabs = document.querySelectorAll(".tab");
+let tabsContent = document.querySelectorAll(".tab__content");
+
+tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        tabs.forEach((item) => {
+            item.classList.contains("tab--active") ? item.classList.remove("tab--active") : null;
+        });
+
+        tabsContent.forEach((item) => {
+            item.classList.contains("tab__content--active") ? item.classList.remove("tab__content--active") : null;
+            item.dataset.id === tab.dataset.id ? item.classList.add("tab__content--active") : null;
+        });
+
+        tab.classList.add("tab--active");
+    });
+});
+
+//плавный переход по якорям
+let anchors = document.querySelectorAll('a[href*="#"]');
+
+function smoothScrol(blockOffsetTop) {
+    let targetPosition = blockOffsetTop;
+    let startPosition = window.pageYOffset;
+    let distance = 0;
+    let speed = 50;
+
+    if (startPosition < targetPosition) {
+        distance = targetPosition - startPosition;
+    } else {
+        distance = startPosition - targetPosition;
+    }
+
+    function animation() {
+        if (distance > 0) {
+            distance -= speed;
+            window.scrollTo(0, startPosition);
+
+            if (startPosition > targetPosition - 50) {
+                startPosition -= speed;
+            } else if (startPosition < targetPosition - 50) {
+                startPosition += speed;
+            }
+
+            requestAnimationFrame(animation);
+        }
+    }
+
+    requestAnimationFrame(animation);
+}
+
+anchors.forEach((anchor) => {
+    anchor.addEventListener("click", (e) => {
+        e.preventDefault();
+        let blockID = anchor.getAttribute("href").substr(1);
+        let blockOffsetTop = document.getElementById(blockID).offsetTop;
+
+        smoothScrol(blockOffsetTop);
+    });
+});
