f02b756a5f40b2e58442accd1d27a443c7b44eaf
Sowa <Grimmino@gmail.com>
2020-06-03 15:32:57 +0500

    refactoring, add images

--- src/index.js
+++ src/index.js
@@ -13,3 +13,11 @@ import imgBig2 from "./img/img-big2.jpg";
 import imgBig3 from "./img/img-big3.jpg";
 import imgBig4 from "./img/img-big4.jpg";
 import imgBig5 from "./img/img-big5.jpg";
+
+import tabs1tab1 from "./img/tabs-1-tab-1.jpg";
+import tabs1tab2 from "./img/tabs-1-tab-2.jpg";
+
+import tabs2tab1 from "./img/tabs-2-tab-1.jpg";
+import tabs2tab2 from "./img/tabs-2-tab-2.jpg";
+import tabs2tab3 from "./img/tabs-2-tab-3.jpg";
+import tabs2tab4 from "./img/tabs-2-tab-4.jpg";
